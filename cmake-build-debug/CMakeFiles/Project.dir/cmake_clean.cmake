a7ce2c513077ab00bbb3b08f8a344bc612f60048
gravestoned <odillner@gmail.com>
2018-03-17 21:32:34 +0100

    initial

---
+++ cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project.dir/src/common.cpp.o"
+  "CMakeFiles/Project.dir/src/main.cpp.o"
+  "CMakeFiles/Project.dir/src/particlematrix.cpp.o"
+  "CMakeFiles/Project.dir/src/particles.cpp.o"
+  "Project.pdb"
+  "Project.exe"
+  "libProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
